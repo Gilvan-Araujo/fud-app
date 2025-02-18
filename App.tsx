db56b824a5dc867147537ebad7893cef966c8dc5
Gilvan Araujo <gilvanjunior@protonmail.com>
2025-02-18 11:29:45 -0300

    feat(routes): add and configure react navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
 import { Text, ThemeProvider } from '@rneui/themed';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
@@ -8,19 +9,21 @@ import { theme } from './src/theme';
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <SafeAreaView
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <StatusBar />
+      <NavigationContainer>
+        <ThemeProvider theme={theme}>
+          <SafeAreaView
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <StatusBar />
 
-          <Text>Universal React with Expo</Text>
-        </SafeAreaView>
-      </ThemeProvider>
+            <Text>Universal React with Expo</Text>
+          </SafeAreaView>
+        </ThemeProvider>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
