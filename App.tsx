c5610f64bd63cca5516a5c51454e81479f08ba1a
Gilvan Araujo <gilvanjunior@protonmail.com>
2025-02-20 10:41:56 -0300

    feat(fonts): add roboto and roboto mono fonts to the project

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
-import { StatusBar } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
 
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+import { RobotoMono_700Bold } from '@expo-google-fonts/roboto-mono';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from '@rneui/themed';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -8,6 +14,14 @@ import { Routes } from './src/routes';
 import { theme } from './src/theme';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    RobotoMono_700Bold,
+  });
+
+  if (!fontsLoaded) return <ActivityIndicator />;
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
