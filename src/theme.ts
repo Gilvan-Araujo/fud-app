a8de11ae9a5d4b31b0bbd1fb6f57a24dbb33be9f
Gilvan Araujo <gilvanjunior@protonmail.com>
2025-02-21 13:21:00 -0300

    refactor(theme): add customization to input component

--- src/theme.ts
+++ src/theme.ts
@@ -4,7 +4,10 @@ export const theme = createTheme({
   mode: 'dark',
   darkColors: {
     primary: '#0080FF',
-    background: '#121212',
+    error: '#FF1000',
+    warning: '##FFA000',
+    disabled: '#A0A0A0',
+    background: '#2B2F33',
   },
   lightColors: {
     primary: '#0080FF',
@@ -24,11 +27,28 @@ export const theme = createTheme({
     },
     Text(props, _theme) {
       return {
+        h3Style: { fontSize: 32 },
         style: {
           ...(props.logo && {
             fontSize: 40,
             fontFamily: 'RobotoMono_700Bold',
           }),
+        },
+      };
+    },
+    Input(_props, theme) {
+      return {
+        placeholderTextColor: theme.colors.disabled,
+        inputStyle: { fontSize: 16 },
+        containerStyle: { paddingLeft: 0, paddingRight: 0 },
+        labelStyle: { color: theme.colors.black },
+        inputContainerStyle: {
+          height: 40,
+          borderWidth: 1,
+          paddingLeft: 8,
+          paddingRight: 8,
+          borderRadius: 10,
+          borderColor: theme.colors.disabled,
         },
       };
     },
